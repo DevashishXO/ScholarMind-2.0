e15ba7dafb8d8747fc98f33441dab9583787dcc2
Miheer Gautam <miheergautam04@gmail.com>
2025-10-16 19:15:36 +0530

    feat: Project init and tailwind setup with custom color theme and fonts
    - Added orange (#d87656), light (#faf9f5), gray (#30302e)
    - Integrated Bungee font

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center  justify-center bg-[var(--color-light)] text-[var(--color-gray)]">
+      <h1 className="text-7xl font-bold text-[var(--color-orange)]">
+        Scholar Mind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
