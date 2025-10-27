222438086f74435bcae67a081ee725787cc59581
Miheer Gautam <miheergautam04@gmail.com>
2025-10-28 03:53:13 +0530

    feat(profile): Add profile routes for create, update and authenticate the user profile

--- backend/app/routes/v1/__init__.py
+++ backend/app/routes/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .auth_routes import router as auth_router
+from .profile_routes import router as profile_router
 
 api_router = APIRouter()
 
 # Include routes
-api_router.include_router(auth_router, prefix="/auth")
+api_router.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(profile_router, prefix="/profile", tags=["Profile"])
