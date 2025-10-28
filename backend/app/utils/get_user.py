ebb3a542f4e1533a7ba6941010a21aa41b1f07a4
Miheer Gautam <miheergautam04@gmail.com>
2025-10-28 14:31:18 +0530

    fix: used sub inplace of userId

--- backend/app/utils/get_user.py
+++ backend/app/utils/get_user.py
@@ -1,7 +1,8 @@
 from fastapi import Depends, HTTPException, Request, status
 from jose import jwt, JWTError
-from utils.db import get_db_from_request
-from core.config import settings
+from app.utils.db import get_db_from_request
+from app.core.config import settings
+from bson import ObjectId
 
 async def get_current_user(request: Request):
     token = request.cookies.get("access_token")  
@@ -13,8 +14,8 @@ async def get_current_user(request: Request):
         )
 
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
-        user_id: str = payload.get("user_id")
+        payload = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM])
+        user_id: str = payload.get("sub")
         if user_id is None:
             raise HTTPException(status_code=401, detail="Invalid token")
     except JWTError:
@@ -22,7 +23,8 @@ async def get_current_user(request: Request):
 
     # Get MongoDB instance
     db = get_db_from_request(request)
-    user = await db.users.find_one({"_id": user_id})
+    user = await db.users.find_one({"_id": ObjectId(user_id)})
+    print(user)
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
